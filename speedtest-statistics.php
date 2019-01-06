f087529a604053f6151d90446e9dce2de91fa676
Luc de Louw <luc@delouw.ch>
2019-01-06 11:40:35 +0100

    New feature: Select an end date to display historical data. Cosmetics: Use a nicer font, cleanup header, more compliance to HTML

--- speedtest-statistics.php
+++ speedtest-statistics.php
@@ -4,7 +4,8 @@
 <style>
 body {
        margin: 0;
-       padding: 0
+       padding: 0;
+       font-family: Verdana, Arial, sans-serif; color: #000;
      }
 #myChart {
      position: top;
@@ -12,15 +13,44 @@ body {
      height: 100%; 
    }
 </style> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="utf-8" /> 
 <title>Bandwidth statistics - Up- and downstream speeds in last 24 hours</title>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+	  $( "#datepicker" ).datepicker();
+	  $( "#datepicker" ).datepicker( "option", "dateFormat","yy-mm-dd");
+  } );
+  </script>
 <script src="scripts/Chart.bundle.js"></script>
 </head>
 <body>
-<h3>bandwidth statistics - last 24 hours </h3>
+<h3>Bandwidth statistics - per 24 hours </h3>
 <?php
 
-
+if (!isset($_GET['enddate']) or (empty($_GET['enddate'])))
+{
+        $enddate=date('Y-m-d');
+        }
+else {
+        $enddate=$_GET['enddate'];
+}
+$datetime1 = new DateTime($enddate);
+$datetime2 = new DateTime(date('Y-m-d')); 
+$interval = $datetime1->diff($datetime2);
+$days= $interval->format('%R%a');
+$offset = $days * 48;
+if ($days<0)
+{
+	print "<b>Error: </b>End date is in the future, falling back to most recent data<br>";
+	$enddate = date("Y-m-d");
+	$offset=0;
+	$days=0;
+}
 
 // Get out public IP address, JFYI
 $ip = file_get_contents('https://api.ipify.org');
@@ -34,10 +64,18 @@ else {
 	$serverid=$_GET['serverid'];
 }
 
-print "Displaying statistics for Server ID ".$serverid. "\n<br>";
+print "Target speedtest Server ID is: ".$serverid. "<br>";
+print "End of display: ".$enddate.", offset ".$days * 24 ." hours from now.<br>";
 
 if (!empty($ip)) {
-	print "Your public IPv4 address is ". $ip." ";
+	$hostname = gethostbyaddr($ip);
+	
+	if ($hostname==$ip)
+	{
+		$hostname = "Can not lookup hostname";
+	}
+
+	print "Source public IPv4 address is: ". $ip." Hostname: ". $hostname;
 	$objects=json_decode(file_get_contents("https://api.iptoasn.com/v1/as/ip/".$ip));
 
 	$as[1] = $objects->{'as_number'};
@@ -46,13 +84,21 @@ if (!empty($ip)) {
 	# Disabled as the API has limited use.
 	# $as=str_getcsv(file_get_contents("https://api.hackertarget.com/aslookup/?q=$ip"));
 	print "<br>";
-	print "Autonomous System is <a href=\"https://bgp.he.net/AS".$as[1]."\">"."AS ".$as[1]." ".$as[3]."</a>";
+	print "Source Autonomous System is: <a href=\"https://bgp.he.net/AS".$as[1]."\">"."AS ".$as[1]." ".$as[3]."</a>";
 }
 else {
 	print "IP lookup not possible, check SELinux and firewall settings";
 }
 
 ?>
+
+<form target="_self" name=date>
+<p>End Date: <input type="text" name=enddate value="<?php print $enddate ?>" autocomplete="off" id="datepicker" onchange="this.form.submit()">
+<input type="hidden" name="serverid" id="serverid" value="<?php print $serverid;?>">
+<input type="submit">
+<input type="submit" value="Back to most recent data"></p>
+</form>
+
 
 <canvas id="myChart"></canvas>
 <script>
@@ -75,7 +121,8 @@ else {
 	$tz=exec('date +"%Z"',$tz);
 
 	// Limit the query to the last 48 sample as we collect data every 30 minutes from two servers.
-	$sql = 'SELECT serverid,  strftime("%H:%M-'.$tz.'", times, "localtime") || " " || strftime("%Y-%m-%d", times) AS timestamp, sponsor, servername, download, upload  from (select * FROM bandwidth WHERE serverid='.$serverid.' ORDER BY times DESC LIMIT 48) ORDER BY times ASC;';
+	# $sql = 'SELECT serverid,  strftime("%H:%M-'.$tz.'", times, "localtime") || " " || strftime("%Y-%m-%d", times) AS timestamp, sponsor, servername, download, upload  from (select * FROM bandwidth WHERE serverid='.$serverid.' ORDER BY times DESC LIMIT 48) ORDER BY times ASC;';
+	$sql = 'SELECT serverid,  strftime("%H:%M-'.$tz.'", times, "localtime") || " " || strftime("%Y-%m-%d", times) AS timestamp, sponsor, servername, download, upload from (select * FROM bandwidth WHERE serverid='.$serverid.' ORDER BY times DESC LIMIT 48 OFFSET '.$offset.' ) ORDER BY times ASC;';
 
 	// Execute and return error if unsuccessful
 	$ret = $db->query($sql);
@@ -179,6 +226,7 @@ else {
 
 ?>
 </select>
+<input type="hidden" id="enddate" name="enddate" value="<?php print $enddate;?>">
 <input type="submit">
 </form>
 <font size="2">speedtest-statistics is &copy; 2019 by Luc de Louw <a href="mailto: luc@delouw.ch">luc@delouw.ch</a>. See <a href="https://github.com/ldelouw/speedtest-statistics">https://github.com/ldelouw/speedtest-statistics</a> for more information</font>
